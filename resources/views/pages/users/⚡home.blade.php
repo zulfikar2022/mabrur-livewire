68789b4b1b317ce44b47b3b1acf9e7b1c287b410
Sayed Zulfikar Mahmud <sayedzulfikar2019@gmail.com>
2026-05-31 19:04:02 +0600

    feat: update product grid layout to display five columns for better visibility

--- resources/views/pages/users/⚡home.blade.php
+++ resources/views/pages/users/⚡home.blade.php
@@ -24,7 +24,7 @@ new class () extends Component {
 
 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 my-8 space-y-6">
    
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
         @forelse($this->products as $product)
             <livewire:user.product-card :product="$product" :key="'prod-grid-'.$product->id" />
         @empty
