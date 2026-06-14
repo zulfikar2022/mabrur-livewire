45831b828ca781e373b30330aa00f9668e77f590
Sayed Zulfikar Mahmud <sayedzulfikar2019@gmail.com>
2026-06-14 11:06:38 +0600

    fix: update login page to include a message and translate button text to Bengali

--- resources/views/pages/users/auth/⚡login.blade.php
+++ resources/views/pages/users/auth/⚡login.blade.php
@@ -11,7 +11,8 @@ new class () extends Component {
 };
 ?>
 
-<div class="flex items-center justify-center mt-10">
+<div class="flex flex-col gap-3.5 items-center justify-center mt-10">
+    <p>পণ্য অর্ডার করার জন্য আপনাকে আগে লগইন করতে হবে। </p>
     <button 
         wire:click="redirectToGoogle"
         class="flex items-center gap-3 px-6 py-3 bg-white border border-gray-300 rounded-lg shadow-sm hover:bg-gray-50 hover:cursor-pointer transition duration-200"
@@ -19,6 +20,6 @@ new class () extends Component {
         <!-- The Google Icon -->
        <img src="https://res.cloudinary.com/dq7jdy5xy/image/upload/v1781240987/google_ceygiu.jpg"  class="w-5 h-5" alt="Google Logo">
         
-        <span class="font-semibold text-gray-700">Login with Google</span>
+        <span class="font-semibold text-gray-700">গুগল দিয়ে লগইন করুন</span>
     </button>
 </div>
