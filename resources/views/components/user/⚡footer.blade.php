2bdd555fb56477eef8af38dc1aaec979fb2f10c1
Sayed Zulfikar Mahmud <sayedzulfikar2019@gmail.com>
2026-08-10 17:47:27 +0600

    footer updated

--- resources/views/components/user/⚡footer.blade.php
+++ resources/views/components/user/⚡footer.blade.php
@@ -74,8 +74,7 @@ new class () extends Component {
             <p class="text-center">All rights reserved</p>
         @else
             <div class="container mx-auto text-center text-sm">
-                This website is developed by <a href="https://github.com/zulfikar2022" target="_blank" class="underline">Sayed Zulfikar Mahmud</a> 
-                <!-- and <a href="https://github.com/ADIBA-ANJUM-HSTU" target="_blank" class="underline">Adiba Anjum</a>  -->
+                This website is developed by <a href="https://github.com/zulfikar2022" target="_blank" class="underline">Sayed Zulfikar Mahmud and <a href="https://github.com/ADIBA-ANJUM-HSTU" target="_blank" class="underline">Adiba Anjum</a> </a> 
             </div>
         @endif
     </div>
